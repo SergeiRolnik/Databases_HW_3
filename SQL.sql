2c276b4d81d15bdc515d864b48df7b6ff6fa84b2
Сергей Рольник <forgedwheels@gmail.com>
2022-03-25 19:32:54 +0300

    DB scheme updated

--- SQL.sql
+++ SQL.sql
@@ -1,49 +1,47 @@
-create table if not exists Исполнители (
-	Идентификатор serial primary key,
-	Имя varchar(50) not null
+create table if not exists Artists(
+	id serial primary key,
+	name varchar(50) not null
 );
 
-create table if not exists Жанры (
-	Идентификатор serial primary key,
-	Название varchar(50) not null,
-	Идентификатор_исполнителя integer references Исполнители(Идентификатор)
+create table if not exists Genres(
+	id serial primary key,
+	name varchar(50) not null
 );
 
-create table if not exists Жанры_Исполнители (
-	Идентификатор_жанра integer references Жанры(Идентификатор),
-	Идентификатор_исполнителя integer references Исполнители(Идентификатор),
-	constraint pk1 primary key (Идентификатор_жанра, Идентификатор_исполнителя)
+create table if not exists Genres_Artists(
+	genre_id integer references Genres(id),
+	artist_id integer references Artists(id),
+	constraint pk1 primary key (genre_id, artist_id)
 );
 
-create table if not exists Альбомы (
-	Идентификатор serial primary key,
-	Название varchar(50) not null,
-	Год_выпуска integer not null, check (Год_выпуска >= 1900 and Год_выпуска <= 2022),
-	Идентификатор_альбома integer references Исполнители(Идентификатор)
+create table if not exists Albums(
+	id serial primary key,
+	name varchar(50) not null,
+	release_year integer not null, check (release_year >= 1900 and release_year <= 2022)
 );
 
-create table if not exists Альбомы_Исполнители (
-	Идентификатор_альбома integer references Альбомы(Идентификатор),
-	Идентификатор_исполнителя integer references Исполнители(Идентификатор),
-	constraint pk2 primary key (Идентификатор_альбома, Идентификатор_исполнителя)
+create table if not exists Albums_Artists(
+	album_id integer references Albums(id),
+	artist_id integer references Artists(id),
+	constraint pk2 primary key (album_id, artist_id)
 );	
 
-create table if not exists Треки (
-	Идентификатор serial primary key,
-	Название varchar(50) not null,
-	Длительность integer not null, check (Длительность > 0 and Длительность <= 600),
-	Идентификатор_альбома integer references Альбомы(Идентификатор)
+create table if not exists Tracks(
+	id serial primary key,
+	name varchar(50) not null,
+	length integer not null, check (length > 0 and length <= 600),
+	album_id integer references Albums(id)
 );
 
-create table if not exists Сборники (
-	Идентификатор serial primary key,
-	Название varchar(50) not null,
-	Год_выпуска integer not null, check (Год_выпуска >= 1900 and Год_выпуска <= 2022)
+create table if not exists Collections(
+	id serial primary key,
+	name varchar(50) not null,
+	release_year integer not null, check (release_year >= 1900 and release_year <= 2022)
 );
 
-create table if not exists Сборники_Треки (
-	Идентификатор_сборника integer references Сборники(Идентификатор),
-	Идентификатор_трека integer references Треки(Идентификатор),
-	constraint pk3 primary key (Идентификатор_сборника, Идентификатор_трека)
+create table if not exists Collections_Tracks(
+	collection_id integer references Collections(id),
+	track_id integer references Tracks(id),
+	constraint pk3 primary key (collection_id, track_id)
 );	
 
